bc7a07786f0836171297f41c4e624e3d31c692fb
JOAO BRUGGER <joaopaulobrugger1991@gmail.com>
2025-04-22 19:51:34 -0300

    altera o server

--- allbooks-aula-01/src/server.js
+++ allbooks-aula-01/src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas vindas a APi al books")
-  console.log("API atraves  em http://localhost:8000")
-=======
-  console.log("acesse essa API em http://localhost:8000")
->>>>>>> 721ad76015f7986f20602a6e3642b4128cf23f49
+  console.log("API disponivel em http://localhost:8000")
 })
